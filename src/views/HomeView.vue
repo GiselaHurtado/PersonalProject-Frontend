9a295871e4bb8cae1bc9b8a81188d5f752414a75
Gisela Soledad Hurtado <hurtadogiselasoledad@gmail.com>
2024-09-18 19:52:11 +0200

    structure

---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<script setup>
+import Login from '../components/Login.vue';
+import Register from '../components/Register.vue';
+
+</script>
+
+<template>
+  <main>
+    <Register />
+    <Login />
+  </main>
+</template>
